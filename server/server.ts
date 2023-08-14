cac6235c8aab76ade782ab90fe5c782db7cbf73c
zivZarum <124026458+zivZarum@users.noreply.github.com>
2023-08-14 23:08:10 +0300

    create server ,email,mongoBd
    All the details in file gitignore
    Email.
    Mongodb.

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,35 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import express  from "express";
 
-dotenv.config();
-
+require("dotenv").config();
 const app = express();
-const port = process.env.PORT|| 8000;
+const mongoose=require ("mongoose");
+const cors = require("cors");
 
-app.get('/', (req:any, res:any) => {
+const port = process.env.PORT || 8000;
+
+app.use(cors())
+app.use(express.json());
+
+export default async function connectDB(){
+  mongoose.connect(process.env.MONGO_URI).then(()=>{
+  console.log('Connected to mongoDB')
+  }).catch((err:any)=>{
+      console.log(err);
+  })
+}
+connectDB();
+
+app.get('/homepage', (req:any, res:any) => {
+  try{
+    res.status(200).send('welcome');
+  }catch{
+    return res.status(500).send({message:'server error'})
+  }
   res.send('Express + TypeScript Server');
 });
 
+
+
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`Server is running at port:${port}`);
 });
