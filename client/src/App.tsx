7395750cc362df442f2bb1bf30043477d6afb805
ilanhg <127044655+ilanhg@users.noreply.github.com>
2023-08-14 14:26:53 +0300

    added register and login pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Routes, Navigate } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
-   <div>
-
-   </div>
+    <>
+      <Routes>
+        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/homepage" />} />
+      </Routes>
+    </>
   );
 }
 
