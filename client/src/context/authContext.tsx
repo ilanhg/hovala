1deec326476205c1d0f93fd820ddd1ffe3ad44f4
ilanhg <127044655+ilanhg@users.noreply.github.com>
2023-08-31 22:24:35 +0300

    a

--- client/src/context/authContext.tsx
+++ client/src/context/authContext.tsx
@@ -1,8 +1,14 @@
-import React, { createContext, useLayoutEffect } from "react";
+import React, {
+  createContext,
+  useCallback,
+  useLayoutEffect,
+  useState,
+} from "react";
 import axios from "axios";
-const isLoggedIn: boolean = false;
+import { useNavigate } from "react-router-dom";
+let isLoggedIn: boolean;
 
-export const authContex = createContext(isLoggedIn);
+export const AuthContext = createContext({ isLoggedIn: false });
 
 function checkToken() {
   const isAccessToken = window.localStorage.getItem("accessToken");
@@ -10,36 +16,45 @@ function checkToken() {
     isLoggedIn: isAccessToken ? true : false,
   };
 }
+const isAuth:any = function ({ children }: { children: any}) {
+  const navigate = useNavigate();
 
-
-useLayoutEffect(() => {
+  const logout = useCallback(() => {
+    isLoggedIn = false;
+    window.localStorage.removeItem("accessToken");
+    window.localStorage.removeItem("refreshToken");
+    navigate("/login");
+  }, [isLoggedIn]);
+  useLayoutEffect(() => {
     const requestsInterceptor = axios.create().interceptors.request.use(
       (req) => {
-        const accessToken = window.localStorage.getItem('accessToken');
+        const accessToken = window.localStorage.getItem("accessToken");
         if (accessToken) {
           req.headers.Authorization = `Bearer ${accessToken}`;
         }
         return req;
       },
-      (error: Error) => error,
+      (error: Error) => error
     );
 
     const responsesInterceptor = axios.create().interceptors.response.use(
       (res) => res,
       async (error: Error & { response: any }) => {
         if (error?.response?.status === 401) {
-          const refreshToken = window.localStorage.getItem('refreshToken');
+          const refreshToken = window.localStorage.getItem("refreshToken");
           if (refreshToken) {
-            const response = await axios.create().post('http://localhost:4000/token', { refreshToken });
+            const response = await axios
+              .create()
+              .post("http://localhost:4000/token", { refreshToken });
             const accessToken = response?.data?.accessToken;
             if (accessToken) {
-              window.localStorage.setItem('accessToken', accessToken);
+              window.localStorage.setItem("accessToken", accessToken);
             } else {
-              // logout();
+              logout();
             }
           } else {
             console.log(error);
-            // logout();
+            logout();
           }
         }
         alert(`Error from server, status: ${error?.response?.status}`);
@@ -49,5 +64,20 @@ useLayoutEffect(() => {
     return () => {
       axios.create().interceptors.request.eject(requestsInterceptor);
       axios.create().interceptors.response.eject(responsesInterceptor);
-    }
+    };
   }, [isLoggedIn]);
+  return <>{children}</>;
+};
+
+const authProvider = ({ children }: { children: any }) => {
+  const [loggedIn, setLoggedIn] = useState(checkToken());
+  return (
+    <AuthContext.Provider value={{ isLoggedIn }}>
+      <isAuth>
+        {children}
+      </isAuth>
+    </AuthContext.Provider>
+  );
+};
+export { useAuthContext };
+export default authProvider;
